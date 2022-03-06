19e2bb69ee864a48bbff03d45f51a98160a4e857
Shreyas Pargaonkar <shreyaspargaonkar@gmail.com>
2022-03-07 02:56:26 +0530

    first

---
+++ src/navigation/BottomTabNavigator.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens/HomeScreen";
+
+
+import { Entypo, FontAwesome, Foundation } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "grey",
+        tabBarStyle: {
+          backgroundColor: "#181818",
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ focused, color }) => (
+            <Entypo name="home" size={focused ? 30 : 25} color={color} />
+          ),
+        }}
+      />
+    
+     
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTabNavigator;
